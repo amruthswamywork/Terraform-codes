a2b9b5499cfa4a17ea3758dad0e4312e0d141c89
AMRUTH SWAMY C P <amruthswamy11@gmail.com>
2026-03-02 16:31:40 +0530

    vpc added

---
+++ create-vpc.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+region ="ap-south-2"
+}
+resource "aws_vpc" "my_vpc" {
+cidr_block = " "
+tags = {
+Name = "my_vpc1"
+}
+resource "aws_subnet" "public_subnet" {
+vpc_id = "aws-vpc.my_vpc.id "
+cidr_block = "19203o3/.3933,303"   #ip address
+map_public_id_on_launch = true
+availablity_zone = "ap-south=1"
+tags ={
+    Name = "public_subnet"
+} 
+}
